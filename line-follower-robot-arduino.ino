7ebf0b6828daedbeeeee916de9ccbffdfebbcc66
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2017-03-29 01:23:34 +0300

    New Algorithm - New Function
    *The new algorithm provide to reverse motor turn.
    **The move() function provide to us to use motors easly.
    ***This algorithm doesn't work very well but the robot never go out of
    the line.

--- line-follower-robot-arduino.ino
+++ line-follower-robot-arduino.ino
@@ -11,25 +11,58 @@
 // paper and sliding the sensor across it.  It prints the sensor values to the serial 
 // monitor as numbers from 0 (maximum reflectance) to 1023 (minimum reflectance).
 
-#define Kp 0.1 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kp 0.7 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
 #define Kd 1.5 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
 #define rightMaxSpeed           250 // max speed of the robot
 #define leftMaxSpeed            250 // max speed of the robot
-#define rightBaseSpeed          200 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed           200  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define rightBaseSpeed          250 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed           250  // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define NUM_SENSORS             8  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  8  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             2  // emitter is controlled by digital pin 2
-#define RIGHT_SPEED             10 // sağ motor PWM hız kontrol pini.    
-#define LEFT_SPEED              9  // sol motor PWM hız kontrol pini.
+#define RIGHT_MOTOR_SPEED       10 // sağ motor PWM hız kontrol pini.    
+#define LEFT_MOTOR_SPEED        9  // sol motor PWM hız kontrol pini.
 #define RIGHT_MOTOR_FORWARD     7
 #define RIGHT_MOTOR_BACKWARD    6
 #define LEFT_MOTOR_FORWARD      5
-#define LEFT_MOTOR_BACKWARD     4            
+#define LEFT_MOTOR_BACKWARD     4   
+#define FORWARD                 1
+#define BACKWARD                0
+#define RIGHT_MOTOR             0
+#define LEFT_MOTOR              1
 
-// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
+
+void move(unsigned char selectMotor, unsigned char selectMovement, int selectMotorSpeed)
+{
+  if(selectMotor == RIGHT_MOTOR) {
+    if(selectMovement == FORWARD) {
+      digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
+      digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
+      analogWrite(RIGHT_MOTOR_SPEED, selectMotorSpeed);
+      }
+    if(selectMovement == BACKWARD) {
+      digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+      digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
+      analogWrite(RIGHT_MOTOR_SPEED, -1*selectMotorSpeed);
+      }
+  }
+  else {
+    if(selectMovement == FORWARD) {
+      digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
+      digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
+      analogWrite(LEFT_MOTOR_SPEED, selectMotorSpeed);
+      }
+    if(selectMovement == BACKWARD) {
+      digitalWrite(LEFT_MOTOR_FORWARD, LOW);
+      digitalWrite(LEFT_MOTOR_BACKWARD, HIGH);
+      analogWrite(LEFT_MOTOR_SPEED, -1*selectMotorSpeed);
+      }
+    }
+  }
+// sensors 0 through 7 are connected to analog inputs 7 through 0, respectively
 QTRSensorsAnalog qtra((unsigned char[]) {7, 6, 5, 4, 3, 2, 1, 0}, 
-  NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+                      NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+                      
 unsigned int sensorValues[NUM_SENSORS], a, Output[NUM_SENSORS];
 
 
@@ -40,8 +73,8 @@ void setup()
   Serial.begin(9600); // set the data rate in bits per second for serial data transmission
   delay(1000);
   
-  pinMode(RIGHT_SPEED,OUTPUT);
-  pinMode(LEFT_SPEED,OUTPUT);
+  pinMode(RIGHT_MOTOR_SPEED,OUTPUT);
+  pinMode(LEFT_MOTOR_SPEED,OUTPUT);
   pinMode(RIGHT_MOTOR_FORWARD,OUTPUT);
   pinMode(RIGHT_MOTOR_BACKWARD,OUTPUT);
   pinMode(LEFT_MOTOR_FORWARD,OUTPUT);
@@ -56,74 +89,67 @@ void loop()
 
   int position = qtra.readLine(sensorValues);
   int error = position - 3500;
-
-  /*
-  for(unsigned char i = 0; i < 8; i ++){
-    Serial.print(i+1);
-    Serial.print(": ");
-    Serial.print(sensorValues[i]);
-    Serial.print("  ");
-    }
-    
-    Serial.println();
-    */
-
-    //Serial.println(position);
-
     
   int motorSpeed = Kp * error + Kd * (error - lastError);
   lastError = error;
 
-  int rightMotorSpeed = rightBaseSpeed + motorSpeed;
-  int leftMotorSpeed = leftBaseSpeed - motorSpeed;
+  int leftMotorSpeed = rightBaseSpeed + motorSpeed;
+  int rightMotorSpeed = leftBaseSpeed - motorSpeed;
   
-  if (rightMotorSpeed > rightMaxSpeed ) 
-      rightMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
-  if (leftMotorSpeed > leftMaxSpeed ) 
-      leftMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
+  if (leftMotorSpeed > rightMaxSpeed ) 
+      leftMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
+  if (rightMotorSpeed > leftMaxSpeed ) 
+      rightMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
       
-  if (rightMotorSpeed < 0) 
-      rightMotorSpeed = 0; // keep the motor speed positive
-  if (leftMotorSpeed < 0) 
-      leftMotorSpeed = 0; // keep the motor speed positive
+  if (leftMotorSpeed < -1*leftMaxSpeed) 
+      leftMotorSpeed = -1*leftMaxSpeed; // keep the motor speed positive
+  if (rightMotorSpeed < -1*rightMaxSpeed) 
+      rightMotorSpeed = -1*rightMaxSpeed; // keep the motor speed positive
+
   /*
-  Serial.print("right: ");
-  Serial.print(rightMotorSpeed);
   Serial.print("  left: ");
   Serial.println(leftMotorSpeed);
-  delay(100);
+  Serial.print("right: ");
+  Serial.print(rightMotorSpeed);
   */
-  analogWrite(RIGHT_SPEED, leftMotorSpeed);
-  analogWrite(LEFT_SPEED, rightMotorSpeed);
+
+  //********** For moving motors **********
+  
+    //move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+    //move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+
+    if(leftMotorSpeed < -100) {
+    move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+    move(LEFT_MOTOR, BACKWARD, leftMotorSpeed);
+    }
+    else if(rightMotorSpeed < -100) {
+      move(RIGHT_MOTOR, BACKWARD, rightMotorSpeed);
+      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+    }
+    else if(rightMotorSpeed > 0 && leftMotorSpeed > 0) {
+      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+      }
+    else if(leftMotorSpeed < 0) {
+      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+      move(LEFT_MOTOR, BACKWARD, 0);
+    }
+    else if(rightMotorSpeed < 0) {
+      move(RIGHT_MOTOR, BACKWARD, 0);
+      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+    }
+    else {
+      move(RIGHT_MOTOR, FORWARD, 0);
+      move(LEFT_MOTOR, FORWARD, 0);
+      }
+    
+  /*
+  analogWrite(LEFT_MOTOR_SPEED, leftMotorSpeed);
+  analogWrite(RIGHT_MOTOR_SPEED, rightMotorSpeed);
   digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
   digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
   digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
   digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-  
-  // print the sensor values as numbers from 0 to 1023, where 0 means maximum reflectance and
-  // 1023 means minimum reflectance
-  /*
-  for (unsigned char i = 0; i < NUM_SENSORS; i++)
-  {
-    if(sensorValues[i] > 200)
-      a = 1;
-    else
-      a = 0;
-    Serial.print(a);
-    Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
-  }
-  Serial.println();
-  
-  delay(250);
-  */
-  /*
-  for (unsigned char i = 0; i < NUM_SENSORS; i++)
-  {
-    if(sensorValues[i] > 200)
-      Output[i] = 1;
-    else
-      Output[i] = 0;
-  }
   */
   
 }
