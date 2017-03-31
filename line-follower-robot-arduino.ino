2adadac9d141169b86b626693f9fd8c46577bec2
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2017-03-31 03:31:19 +0300

    HC SR04
    Using HC SR04...

--- line-follower-robot-arduino.ino
+++ line-follower-robot-arduino.ino
@@ -11,12 +11,12 @@
 // paper and sliding the sensor across it.  It prints the sensor values to the serial 
 // monitor as numbers from 0 (maximum reflectance) to 1023 (minimum reflectance).
 
-#define Kp 0.7 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 1.5 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define Kp 0.15 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 1 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
 #define rightMaxSpeed           250 // max speed of the robot
 #define leftMaxSpeed            250 // max speed of the robot
-#define rightBaseSpeed          250 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed           250  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define rightBaseSpeed          200 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed           200  // this is the speed at which the motors should spin when the robot is perfectly on the line
 #define NUM_SENSORS             8  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  8  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             2  // emitter is controlled by digital pin 2
@@ -30,6 +30,8 @@
 #define BACKWARD                0
 #define RIGHT_MOTOR             0
 #define LEFT_MOTOR              1
+#define trigPin                 12
+#define echoPin                 11
 
 
 void move(unsigned char selectMotor, unsigned char selectMovement, int selectMotorSpeed)
@@ -79,9 +81,12 @@ void setup()
   pinMode(RIGHT_MOTOR_BACKWARD,OUTPUT);
   pinMode(LEFT_MOTOR_FORWARD,OUTPUT);
   pinMode(LEFT_MOTOR_BACKWARD,OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 int lastError = 0;
+long sure, mesafe;
 void loop()
 {
   // read raw sensor values
@@ -106,50 +111,98 @@ void loop()
   if (rightMotorSpeed < -1*rightMaxSpeed) 
       rightMotorSpeed = -1*rightMaxSpeed; // keep the motor speed positive
 
-  /*
-  Serial.print("  left: ");
-  Serial.println(leftMotorSpeed);
-  Serial.print("right: ");
-  Serial.print(rightMotorSpeed);
-  */
 
-  //********** For moving motors **********
   
-    //move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-    //move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  sure = pulseIn(echoPin, HIGH);
+  mesafe = (sure/2) / 29.1;
+  //Serial.println(mesafe);
+  
+     while (mesafe < 15){
+      
+      leftMotorSpeed = 0;
+      rightMotorSpeed = 0;
+      delay(100);
+      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
 
-    if(leftMotorSpeed < -100) {
-    move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-    move(LEFT_MOTOR, BACKWARD, leftMotorSpeed);
-    }
-    else if(rightMotorSpeed < -100) {
-      move(RIGHT_MOTOR, BACKWARD, rightMotorSpeed);
-      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-    }
-    else if(rightMotorSpeed > 0 && leftMotorSpeed > 0) {
-      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-      }
-    else if(leftMotorSpeed < 0) {
-      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-      move(LEFT_MOTOR, BACKWARD, 0);
-    }
-    else if(rightMotorSpeed < 0) {
-      move(RIGHT_MOTOR, BACKWARD, 0);
-      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-    }
-    else {
-      move(RIGHT_MOTOR, FORWARD, 0);
-      move(LEFT_MOTOR, FORWARD, 0);
-      }
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      sure = pulseIn(echoPin, HIGH);
+      mesafe = (sure/2) / 29.1;
+      
+     }
+      /*
+      Serial.print("  left: ");
+      Serial.println(leftMotorSpeed);
+      Serial.print("right: ");
+      Serial.print(rightMotorSpeed);
+      */
     
-  /*
-  analogWrite(LEFT_MOTOR_SPEED, leftMotorSpeed);
-  analogWrite(RIGHT_MOTOR_SPEED, rightMotorSpeed);
-  digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
-  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
-  digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
-  digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-  */
+      /*
+      for(unsigned char i = 0; i < 8; i ++){
+         Serial.print(i+1);
+         Serial.print(": ");
+         Serial.print(sensorValues[i]);
+         Serial.print("  ");
+         }
+         
+         Serial.println();
+      */
+    
+      //********** For moving motors **********
+      
+        //move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+        //move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+    
+        if(leftMotorSpeed < 0) {
+          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+          move(LEFT_MOTOR, BACKWARD, leftMotorSpeed);
+        }
+        else if(rightMotorSpeed < 0) {
+          move(RIGHT_MOTOR, BACKWARD, rightMotorSpeed);
+          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+        }
+        else if(rightMotorSpeed > 0 && leftMotorSpeed > 0) {
+          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+          }/*
+        else if(leftMotorSpeed < 0) {
+          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+          move(LEFT_MOTOR, BACKWARD, 0);
+        }
+        else if(rightMotorSpeed < 0) {
+          move(RIGHT_MOTOR, BACKWARD, 0);
+          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+        }*/
+        else {
+          move(RIGHT_MOTOR, FORWARD, 100);
+          move(LEFT_MOTOR, FORWARD, 100);
+          }
+        
+      /*
+      analogWrite(LEFT_MOTOR_SPEED, leftMotorSpeed);
+      analogWrite(RIGHT_MOTOR_SPEED, rightMotorSpeed);
+      digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
+      digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
+      digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
+      digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
+      */
+      /*
+      Serial.print("  left: ");
+      Serial.print(leftMotorSpeed);
+      Serial.print("    right: ");
+      Serial.print(rightMotorSpeed);
+      Serial.print("    mesafe: ");
+      Serial.print(mesafe);
+      Serial.println();
+      */
   
 }
