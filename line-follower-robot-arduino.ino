cfb1379d00a0744cee954eca6b97e5e7283501ff
Bahtiyar Bayram <bahtiyar.bayramov@gmail.com>
2017-04-01 19:29:15 +0300

    Clear
    More clear codes...

--- line-follower-robot-arduino.ino
+++ line-follower-robot-arduino.ino
@@ -1,208 +1,181 @@
- #include <QTRSensors.h>
+#include <QTRSensors.h>
 
-// This example is designed for use with six QTR-1A sensors or the first six sensors of a
-// QTR-8A module.  These reflectance sensors should be connected to analog inputs 0 to 5.
-// The QTR-8A's emitter control pin (LEDON) can optionally be connected to digital pin 2, 
-// or you can leave it disconnected and change the EMITTER_PIN #define below from 2 to 
-// QTR_NO_EMITTER_PIN.
+//*********** Motorlar, motorların hızları ve PID algoritması için ***********
 
-// The main loop of the example reads the raw sensor values (uncalibrated).
-// You can test this by taping a piece of 3/4" black electrical tape to a piece of white 
-// paper and sliding the sensor across it.  It prints the sensor values to the serial 
-// monitor as numbers from 0 (maximum reflectance) to 1023 (minimum reflectance).
+#define                         Kp 0.15 // Kp < Kd olmalı.
+#define                         Kd 1 
+#define maxSpeed                250     // motorların maksimum hızı
+#define baseSpeed               200     // tam çizgide olduğu zaman motorların hızı
+#define NUM_SENSORS             8       // sensörlerin sayısı
+#define NUM_SAMPLES_PER_SENSOR  8       // her sensör okumasında 8 değer okunur ve bunların ortalaması alınır
+                                        // her bir okuma 100us sürüyor; buna göre toplam 8(sensor)*8*100us = 6.4ms sürüyor.
+#define EMITTER_PIN             2       // emitter 2 nolu dijital pin ile kontrol ediliyor
+#define RIGHT_MOTOR_SPEED       10      // sağ motor PWM hız kontrol pini.    
+#define LEFT_MOTOR_SPEED        9       // sol motor PWM hız kontrol pini.
+#define RIGHT_MOTOR_POSITIVE    7       // sağ motorun artı ucu
+#define RIGHT_MOTOR_NEGATIVE    6       // sağ motorun eksi ucu
+#define LEFT_MOTOR_POSITIVE     5       // sol motorun artı ucu
+#define LEFT_MOTOR_NEGATIVE     4       // sol motorun eksi ucu  
 
-#define Kp 0.15 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
-#define Kd 1 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
-#define rightMaxSpeed           250 // max speed of the robot
-#define leftMaxSpeed            250 // max speed of the robot
-#define rightBaseSpeed          200 // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define leftBaseSpeed           200  // this is the speed at which the motors should spin when the robot is perfectly on the line
-#define NUM_SENSORS             8  // number of sensors used
-#define NUM_SAMPLES_PER_SENSOR  8  // average 4 analog samples per sensor reading
-#define EMITTER_PIN             2  // emitter is controlled by digital pin 2
-#define RIGHT_MOTOR_SPEED       10 // sağ motor PWM hız kontrol pini.    
-#define LEFT_MOTOR_SPEED        9  // sol motor PWM hız kontrol pini.
-#define RIGHT_MOTOR_FORWARD     7
-#define RIGHT_MOTOR_BACKWARD    6
-#define LEFT_MOTOR_FORWARD      5
-#define LEFT_MOTOR_BACKWARD     4   
+//************** move() fonksiyonu için **************
+
 #define FORWARD                 1
 #define BACKWARD                0
 #define RIGHT_MOTOR             0
 #define LEFT_MOTOR              1
-#define trigPin                 12
-#define echoPin                 11
 
+//********** Mesafe sensörünün kesmesi için **********
+
+#define INTERRUPT_DISTANCE      3
+
+//************** move() fonksiyonu için **************
+
+#define FORWARD                 1
+#define BACKWARD                0
+#define RIGHT_MOTOR             0
+#define LEFT_MOTOR              1
+
+
+void move(unsigned char selectMotor, unsigned char selectMovement, int selectMotorSpeed);
+void stop();
+  
+// 8 sensör sırasıyla 7-0 analog girişlerine bağlanmıştır.
+QTRSensorsAnalog qtra((unsigned char[]) {7, 6, 5, 4, 3, 2, 1, 0}, NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+                      
+// Sensörden gelen analog değerleri tutmak için sensorValues[NUM_SENSORS] dizisi tanımlanmıştır.                      
+unsigned int sensorValues[NUM_SENSORS];
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(2000); // Robot çalışmaya 2 saniye sonra başlar.
+
+  // Motorların ve HC SR04 sensörünün pin durumunun ayarlanması.
+  pinMode(RIGHT_MOTOR_SPEED,OUTPUT);
+  pinMode(LEFT_MOTOR_SPEED,OUTPUT);
+  pinMode(RIGHT_MOTOR_POSITIVE,OUTPUT);
+  pinMode(RIGHT_MOTOR_NEGATIVE,OUTPUT);
+  pinMode(LEFT_MOTOR_POSITIVE,OUTPUT);
+  pinMode(LEFT_MOTOR_NEGATIVE,OUTPUT);
+
+  pinMode(INTERRUPT_DISTANCE, INPUT);
+  //attachInterrupt (digitalPinToInterrupt (INTERRUPT_DISTANCE), stop, LOW);
+}
+
+//PID algoritması için son hata değeri tanımlanıyor.
+int lastError = 0;
+
+void loop()
+{
+  // Tüm sensörleri okur ve okunan değerleri sensorValue[] dizisine atar.
+  qtra.read(sensorValues);
+
+  /*  
+   *  Sensör değerlerini okur ve buna göre bir değer geri dönderir.
+   *  Bu değer 0 ile 7000 arasında bir değerdir. 
+   *  3500 değeri geri dönülürse robot tam çizgi üzerinde anlamına gelir.
+   */
+  int position = qtra.readLine(sensorValues);
+  
+  // Çizginin bulunduğu konuma göre bir hata değeri üretilir.
+  // Eğer robot tam çizginin üzerindeyse değer 3500 olur ve hata 0 olur.
+  int error = position - 3500;
+
+  // PID algoritmasına göre motorun hızı belirlenir.
+  int motorSpeed = Kp * error + Kd * (error - lastError);
+  lastError = error;
+
+  // Sağ ve sol motorun hızları algoritmaya göre düzenlenir.
+  int leftMotorSpeed = baseSpeed + motorSpeed;
+  int rightMotorSpeed = baseSpeed - motorSpeed;
+
+  // Buradaki if ifadeleri motor hızlarının -250 ile +250 arasında kalmasını sağlıyor.
+  if (leftMotorSpeed > maxSpeed ) 
+      leftMotorSpeed = maxSpeed;
+  if (rightMotorSpeed > maxSpeed ) 
+      rightMotorSpeed = maxSpeed;
+      
+  if (leftMotorSpeed < -1*maxSpeed) 
+      leftMotorSpeed = -1*maxSpeed;
+  if (rightMotorSpeed < -1*maxSpeed) 
+      rightMotorSpeed = -1*maxSpeed;
+    
+  //********** Motorların hareketi için **********
+      
+  if(leftMotorSpeed < -100) {
+    move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+    move(LEFT_MOTOR, BACKWARD, leftMotorSpeed);
+  }
+  else if(rightMotorSpeed < -100) {
+    move(RIGHT_MOTOR, BACKWARD, rightMotorSpeed);
+    move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+  }
+  else if(rightMotorSpeed > 0 && leftMotorSpeed > 0) {
+    move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+    move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+    }
+  else if(leftMotorSpeed < 0) {
+    move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
+    move(LEFT_MOTOR, BACKWARD, 0);
+  }
+  else if(rightMotorSpeed < 0) {
+    move(RIGHT_MOTOR, BACKWARD, 0);
+    move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
+  }
+  else {
+    move(RIGHT_MOTOR, FORWARD, 100);
+    move(LEFT_MOTOR, FORWARD, 100);
+    }
+  
+}
+
+//*****************************************************************************************************************************************************************************//
+
+/*
+  move(Motor Seçimi, Yön Seçimi, Hız Değeri)
+  
+  Motor Seçimi: RIGHT_MOTOR = Sağ Motor
+                LEFT_MOTOR  = Sol Motor
+  Y�n Seçimi:   FORWARD     = İleri
+                BACKWARD    = Geri
+  H�z Değeri:  0 ile 255 arasında bir değer alır.
+  
+  Not: Hız değeri negatif olamaz. 
+*/
 
 void move(unsigned char selectMotor, unsigned char selectMovement, int selectMotorSpeed)
 {
   if(selectMotor == RIGHT_MOTOR) {
     if(selectMovement == FORWARD) {
-      digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
-      digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
+      digitalWrite(RIGHT_MOTOR_POSITIVE, HIGH);
+      digitalWrite(RIGHT_MOTOR_NEGATIVE, LOW);
       analogWrite(RIGHT_MOTOR_SPEED, selectMotorSpeed);
       }
     if(selectMovement == BACKWARD) {
-      digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
-      digitalWrite(RIGHT_MOTOR_BACKWARD, HIGH);
+      digitalWrite(RIGHT_MOTOR_POSITIVE, LOW);
+      digitalWrite(RIGHT_MOTOR_NEGATIVE, HIGH);
       analogWrite(RIGHT_MOTOR_SPEED, -1*selectMotorSpeed);
       }
   }
   else {
     if(selectMovement == FORWARD) {
-      digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
-      digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
+      digitalWrite(LEFT_MOTOR_POSITIVE, HIGH);
+      digitalWrite(LEFT_MOTOR_NEGATIVE, LOW);
       analogWrite(LEFT_MOTOR_SPEED, selectMotorSpeed);
       }
     if(selectMovement == BACKWARD) {
-      digitalWrite(LEFT_MOTOR_FORWARD, LOW);
-      digitalWrite(LEFT_MOTOR_BACKWARD, HIGH);
+      digitalWrite(LEFT_MOTOR_POSITIVE, LOW);
+      digitalWrite(LEFT_MOTOR_NEGATIVE, HIGH);
       analogWrite(LEFT_MOTOR_SPEED, -1*selectMotorSpeed);
       }
     }
   }
-// sensors 0 through 7 are connected to analog inputs 7 through 0, respectively
-QTRSensorsAnalog qtra((unsigned char[]) {7, 6, 5, 4, 3, 2, 1, 0}, 
-                      NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
-                      
-unsigned int sensorValues[NUM_SENSORS], a, Output[NUM_SENSORS];
-
-
-void setup()
-{
   
-  delay(500);
-  Serial.begin(9600); // set the data rate in bits per second for serial data transmission
-  delay(1000);
+//*****************************************************************************************************************************************************************************//
+
+  // stop() fonksiyonu robotu durdurmak için kullanılır.
   
-  pinMode(RIGHT_MOTOR_SPEED,OUTPUT);
-  pinMode(LEFT_MOTOR_SPEED,OUTPUT);
-  pinMode(RIGHT_MOTOR_FORWARD,OUTPUT);
-  pinMode(RIGHT_MOTOR_BACKWARD,OUTPUT);
-  pinMode(LEFT_MOTOR_FORWARD,OUTPUT);
-  pinMode(LEFT_MOTOR_BACKWARD,OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-}
-
-int lastError = 0;
-long sure, mesafe;
-void loop()
-{
-  // read raw sensor values
-  qtra.read(sensorValues);
-
-  int position = qtra.readLine(sensorValues);
-  int error = position - 3500;
-    
-  int motorSpeed = Kp * error + Kd * (error - lastError);
-  lastError = error;
-
-  int leftMotorSpeed = rightBaseSpeed + motorSpeed;
-  int rightMotorSpeed = leftBaseSpeed - motorSpeed;
-  
-  if (leftMotorSpeed > rightMaxSpeed ) 
-      leftMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
-  if (rightMotorSpeed > leftMaxSpeed ) 
-      rightMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
-      
-  if (leftMotorSpeed < -1*leftMaxSpeed) 
-      leftMotorSpeed = -1*leftMaxSpeed; // keep the motor speed positive
-  if (rightMotorSpeed < -1*rightMaxSpeed) 
-      rightMotorSpeed = -1*rightMaxSpeed; // keep the motor speed positive
-
-
-  
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  sure = pulseIn(echoPin, HIGH);
-  mesafe = (sure/2) / 29.1;
-  //Serial.println(mesafe);
-  
-     while (mesafe < 15){
-      
-      leftMotorSpeed = 0;
-      rightMotorSpeed = 0;
-      delay(100);
-      move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-      move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-
-      digitalWrite(trigPin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(trigPin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(trigPin, LOW);
-      sure = pulseIn(echoPin, HIGH);
-      mesafe = (sure/2) / 29.1;
-      
-     }
-      /*
-      Serial.print("  left: ");
-      Serial.println(leftMotorSpeed);
-      Serial.print("right: ");
-      Serial.print(rightMotorSpeed);
-      */
-    
-      /*
-      for(unsigned char i = 0; i < 8; i ++){
-         Serial.print(i+1);
-         Serial.print(": ");
-         Serial.print(sensorValues[i]);
-         Serial.print("  ");
-         }
-         
-         Serial.println();
-      */
-    
-      //********** For moving motors **********
-      
-        //move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-        //move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-    
-        if(leftMotorSpeed < 0) {
-          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-          move(LEFT_MOTOR, BACKWARD, leftMotorSpeed);
-        }
-        else if(rightMotorSpeed < 0) {
-          move(RIGHT_MOTOR, BACKWARD, rightMotorSpeed);
-          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-        }
-        else if(rightMotorSpeed > 0 && leftMotorSpeed > 0) {
-          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-          }/*
-        else if(leftMotorSpeed < 0) {
-          move(RIGHT_MOTOR, FORWARD, rightMotorSpeed);
-          move(LEFT_MOTOR, BACKWARD, 0);
-        }
-        else if(rightMotorSpeed < 0) {
-          move(RIGHT_MOTOR, BACKWARD, 0);
-          move(LEFT_MOTOR, FORWARD, leftMotorSpeed);
-        }*/
-        else {
-          move(RIGHT_MOTOR, FORWARD, 100);
-          move(LEFT_MOTOR, FORWARD, 100);
-          }
-        
-      /*
-      analogWrite(LEFT_MOTOR_SPEED, leftMotorSpeed);
-      analogWrite(RIGHT_MOTOR_SPEED, rightMotorSpeed);
-      digitalWrite(RIGHT_MOTOR_FORWARD, HIGH);
-      digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
-      digitalWrite(LEFT_MOTOR_FORWARD, HIGH);
-      digitalWrite(LEFT_MOTOR_BACKWARD, LOW);
-      */
-      /*
-      Serial.print("  left: ");
-      Serial.print(leftMotorSpeed);
-      Serial.print("    right: ");
-      Serial.print(rightMotorSpeed);
-      Serial.print("    mesafe: ");
-      Serial.print(mesafe);
-      Serial.println();
-      */
-  
-}
+  void stop() {
+    move(RIGHT_MOTOR, FORWARD, 0);
+    move(LEFT_MOTOR, FORWARD, 0);
+  }
